8178181032200ab41dbc87adfbb4dabd4ff8449a
Tomasz Turkiewicz <tturkiewicz83@gmail.com>
2019-05-11 16:06:40 +0100

    ciąg Fibonacciego

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -6,8 +6,22 @@ package com.sda.algorytmy;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        int b=4;
+
+        System.out.println(ciagFibonacciego(b));
+
+    }
+    public static int ciagFibonacciego(int a){
+        if (a==1){
+            return 1;
+        }
+        else if (a==2){
+            return 2;
+        }
+        else {
+            return ciagFibonacciego(a-1)+ciagFibonacciego(a-2);
+        }
     }
 }
