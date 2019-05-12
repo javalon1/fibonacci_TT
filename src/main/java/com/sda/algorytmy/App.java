5560ba21763c47dfda0b984ece864fab27c430f0
Tomasz Turkiewicz <tturkiewicz83@gmail.com>
2019-05-12 09:18:04 +0100

    poprawiony ciąg fibonacciego

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -15,7 +15,7 @@ public class App
     }
     public static int ciagFibonacciego(int a){
         if (a==1){
-            return 1;
+            return 0;
         }
         else if (a==2){
             return 2;
