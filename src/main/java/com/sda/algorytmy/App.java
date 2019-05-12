e7274c859309f80006ba3cdd889355c70fce856b
Tomasz Turkiewicz <tturkiewicz83@gmail.com>
2019-05-12 09:38:51 +0100

    fibonacci iteracyjnie

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,27 +1,29 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
 
-        int b=4;
-
+   /*     int b=4;
         System.out.println(ciagFibonacciego(b));
+        */
+        long tab[] = new long[50];
+        tab[0] = 0;
+        tab[1] = 1;
+        for (int i = 2; i < tab.length; i++) {
+            tab[i] = tab[i - 1] + tab[i - 2];
+        }
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println(tab[i]);
+        }
+     /*   public static int ciagFibonacciego ( int a){
+            if (a == 1) {
+                return 0;
+            } else if (a == 2) {
+                return 1;
+            } else {
+                return ciagFibonacciego(a - 1) + ciagFibonacciego(a - 2);
+            }*/
 
-    }
-    public static int ciagFibonacciego(int a){
-        if (a==1){
-            return 0;
-        }
-        else if (a==2){
-            return 1;
-        }
-        else {
-            return ciagFibonacciego(a-1)+ciagFibonacciego(a-2);
         }
     }
-}
+
